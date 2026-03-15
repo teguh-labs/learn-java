a2199a7ee25ce48880c370210f54b01c49961acf
Teguh Prayoga <teguhprayoga33@yahoo.com>
2026-03-15 15:09:51 +0700

    Update class Person nambah method baru

--- src/oop/PersonApp.java
+++ src/oop/PersonApp.java
@@ -5,18 +5,29 @@ public class PersonApp {
         //Cara 1 (lebih modern)
         var person1 = new Person();
 
-        person1.name = "Budi";
-        person1.address = "Sriraya";
+        person1.name = "Budi Nugraha";
+        person1.address = "Jl. Sriraya";
 
         System.out.println(person1.name);
         System.out.println(person1.address);
         System.out.println(person1.country);
-
+        
         //Cara 2
         Person person2 = new Person();
+        person2.name = "Joko Nugroho";
+        person2.address = "Jl. Nusantara";
+        
+        System.out.println(person2.name);
+        System.out.println(person2.address);
+        System.out.println(person2.country);
+        
+        
+        Person person3 = new Person();
+        person3.name = "Eko Kurniawan";
+        person3.address = "Subang";
 
-        //Cara 3
-        Person person3;
-        new Person();
+        System.out.println(person3.name);
+        System.out.println(person3.address);
+        System.out.println(person3.country);
     }
 }
