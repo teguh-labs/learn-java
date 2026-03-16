44e887987e119196603c38973503add7349396f6
Teguh Prayoga <teguhprayoga33@yahoo.com>
2026-03-16 10:03:51 +0700

    Update materi

--- src/oop/Person.java
+++ src/oop/Person.java
@@ -4,4 +4,24 @@ public class Person {
     String name;
     String address;
     final String country = "Indonesia";
+
+    //Constructor 1
+    Person(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+
+    //Constructor 2
+    Person(String paramName){
+        this(paramName, null);
+    }
+
+    //Constructor 3
+    Person(){
+        this(null);
+    }
+
+    void sayHello(String name){
+        System.out.println("Hello " + name + ", my name is " + this.name);
+    }
 }
--- src/oop/PersonApp.java
+++ src/oop/PersonApp.java
@@ -2,32 +2,33 @@ package oop;
 
 public class PersonApp {
     public static void main(String[] args) {
+
         //Cara 1 (lebih modern)
-        var person1 = new Person();
-
-        person1.name = "Budi Nugraha";
-        person1.address = "Jl. Sriraya";
-
+        var person1 = new Person("Budi", "Jl. Nusantara");
+        
         System.out.println(person1.name);
         System.out.println(person1.address);
         System.out.println(person1.country);
+        person1.sayHello("Ilham");
         
         //Cara 2
-        Person person2 = new Person();
-        person2.name = "Joko Nugroho";
-        person2.address = "Jl. Nusantara";
+        Person person2 = new Person("Joko");
+        person2.address = "Jl. Sukajaya";
         
         System.out.println(person2.name);
         System.out.println(person2.address);
         System.out.println(person2.country);
+        person2.sayHello("Rian");
         
-        
-        Person person3 = new Person();
-        person3.name = "Eko Kurniawan";
-        person3.address = "Subang";
+        //Cara 3
+        Person person3;
+        person3 = new Person();
+        person3.name = "Eko";
+        person3.address = "Jl. Sentosa";
 
         System.out.println(person3.name);
         System.out.println(person3.address);
         System.out.println(person3.country);
+        person3.sayHello("Ridho");
     }
 }
